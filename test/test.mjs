d9a3e6ef450c20877006cbb8c78337755e68844e
unit-404 <elviras9t@gmail.com>
2023-08-06 00:06:35 +0800

    DataView is back

--- test/test.mjs
+++ test/test.mjs
@@ -8,13 +8,13 @@ const Test = B.StructWrap(new B.StructLayout("Test", {
 //
 const AB = new ArrayBuffer(256);
 
-const I = new Test(AB);/*{
+const I = new Test({
     field0: 1,
     field1: [2, 3]
-});*/
+});
 
-I.field0 = 1;
-I.field1 = [2, 3];
+//I.field0 = 1;
+//I.field1 = [2, 3];
 
 console.log(I);
 console.log(I.field0);
