7f867aa1430086a2b82daf9cf5435646e6ac6209
unit-404 <elviras9t@gmail.com>
2023-08-05 23:52:17 +0800

    usable with WASM memory

--- test/test.mjs
+++ test/test.mjs
@@ -5,11 +5,18 @@ const Test = B.StructWrap(new B.StructLayout("Test", {
     field1: "uint8[2](1)"
 }, 4));
 
-const I = new Test({
+//
+const AB = new ArrayBuffer(256);
+
+const I = new Test(AB);/*{
     field0: 1,
     field1: [2, 3]
-});
+});*/
+
+I.field0 = 1;
+I.field1 = [2, 3];
 
 console.log(I);
 console.log(I.field0);
 console.log(I.field1[0], I.field1[1]);
+console.log(I.field1);
