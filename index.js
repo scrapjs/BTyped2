21008271b29cdde4ae91999534be1efb8cc9f062
unit-404 <elviras9t@gmail.com>
2023-08-05 22:00:59 +0800

    objectism

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ import StructLayout from "./lib/StructLayout.mjs";
 import ArrayView from "./lib/ArrayView.mjs";
 import StructView from "./lib/StructView.mjs";
 import ProxyHandle from "./lib/ProxyHandle.mjs";
+import StructType from "./lib/StructType.mjs";
 import { StructWrap } from "./lib/ProxyHandle.mjs";
 import MemoryPool from "./lib/MemoryPool.mjs";
 
 //
-export {ArrayLayout, StructLayout, ArrayView, StructView, StructWrap, ProxyHandle, MemoryPool};
+export {ArrayLayout, StructLayout, ArrayView, StructView, StructWrap, ProxyHandle, MemoryPool, StructType};
 export * from "./lib/ArrayLayout.mjs";
