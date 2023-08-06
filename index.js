9822420993198b49a1359bbb752b3843b90cbd83
unit-404 <elviras9t@gmail.com>
2023-08-06 10:08:52 +0800

    dependency

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import StructView from "./lib/StructView.mjs";
 import ProxyHandle from "./lib/ProxyHandle.mjs";
 import StructType from "./lib/StructType.mjs";
 import { StructWrap } from "./lib/ProxyHandle.mjs";
-//import MemoryPool from "./lib/MemoryPool.mjs";
+import MemoryPool from "./lib/MemoryPool.mjs";
 
 //
-export {ArrayLayout, StructLayout, ArrayView, StructView, StructWrap, ProxyHandle, StructType};
+export {ArrayLayout, StructLayout, ArrayView, StructView, StructWrap, ProxyHandle, StructType, MemoryPool};
 export * from "./lib/ArrayLayout.mjs";
