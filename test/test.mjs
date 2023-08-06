0c1f8d52de2ebe93b33fa7f6500a79b302c70eab
unit-404 <elviras9t@gmail.com>
2023-08-06 14:31:23 +0800

    align correctly

--- test/test.mjs
+++ test/test.mjs
@@ -1,13 +1,14 @@
 import * as B from "../index.js";
 
 const Test = B.StructWrap(new B.StructLayout("Test", {
-    field0: "uint8(0)",
-    field1: "uint8[2](1)"
-}, 4));
+    field0: "uint8",
+    field1: "uint8[2]"
+}, 4)).$auto;
 
 //
 const AB = new ArrayBuffer(256);
 
+//
 const I = new Test({
     field0: 1,
     field1: [2, 3]
