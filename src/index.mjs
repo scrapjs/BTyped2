42c6ef66440fd19311492f49d543d7678543d01b
unit-404 <elviras9t@gmail.com>
2023-08-22 15:38:11 +0800

    illegal index

--- src/index.mjs
+++
@@ -1,9 +0,0 @@
-export * from "./js/UI/Elements.mjs";
-
-//
-export * from "~/bootstrap-icons/font/bootstrap-icons.css?inline";
-export * from "./index.css?inline"
-
-//
-export * from "./scss/SkyDesign2.module.scss"
-export * from "./scss/SkyDesign2.mjs"
